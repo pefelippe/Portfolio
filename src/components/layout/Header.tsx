4566d90c3582aa27ec560e61034612643c42f842
pedro felippe <pedfelippe@gmail.com>
2023-04-13 00:46:46 -0300

    refactor: uiuiiu

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,9 +12,15 @@ const Navbar = () => {
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 2, delay: 1 }}
-      className={` font-semibold mx-auto w-full flex max-w-7xl max-xl:px-10
+      className={` font-semibold mx-auto w-full flex max-w-7xl max-xl:px-10 gap-8
        items-center h-[10vh] top-0 z-20 bg-primary`}
     >
+      <Link
+        href="/"
+        className="text-[1.8rem] font-bold  text-white min-w-fit underline decoration-blue "
+      >
+        Pedro Felippe
+      </Link>
       <HamburgerMenu />
     </motion.nav>
   );

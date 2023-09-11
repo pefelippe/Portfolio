c83d79a208e533c308f4b000d27be85466292d6a
pedro felippe <pedfelippe@gmail.com>
2023-09-10 22:52:46 -0300

    color to blue

--- src/content/Hero.tsx
+++ src/content/Hero.tsx
@@ -9,7 +9,7 @@ export default function About() {
       className="relative items-center text-center leading-none  justify-center
     flex flex-col gap-2 md:gap-4  max-w-screen-2xl  mx-auto min-h-[90vh]"
     >
-      <p className="text-[4rem] sm:text-[6rem] lg:text-[10rem] 2xl:text-[14rem]  uppercase font-bold text-yellow-500">
+      <p className="text-[4rem] sm:text-[6rem] lg:text-[10rem] 2xl:text-[14rem]  uppercase font-bold text-blue">
         FULL-STACK DEVELOPER
       </p>
       <p className="text-[1.3rem] md:text-[2rem] max-md:max-w-sm font-extralight text-gray-100 leading-[1.2]">

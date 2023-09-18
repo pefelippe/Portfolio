96f3c34fcfc97911e6df3ed725489b6703b214f0
pedro felippe <pedfelippe@gmail.com>
2023-09-18 02:15:10 -0300

    sim.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import HeroContent from "../src/content/HeroContent";
 
 export default function Home() {
   return (
-    <motion.div className=" flex flex-col px-10 max-w-screen-2xl mx-auto gap-10 pb-10">
+    <motion.div className=" flex flex-col px-10 max-w-screen-2xl mx-auto gap-20 pb-10">
       <HeroContent />
       {/* <ProjectContent /> */}
       <MessageForm />
--- src/content/HeroContent.tsx
+++ src/content/HeroContent.tsx
@@ -9,7 +9,7 @@ export default function About() {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
       className="relative flex flex-col  items-center mx-auto  w-full
-    text-center leading-none  justify-center pt-20 gap-4"
+    text-center leading-none  justify-center pt-40 gap-4"
     >
       <div className=" flex flex-col text-center items-center ">
         <p className="text-[1.4rem]   text-gray-100 tracking-[5px] md:tracking-[10px] font-normal mb-4 ">
@@ -21,7 +21,7 @@ export default function About() {
         >
           Pedro Felippe
         </p>
-        <p className="max-w-lg  text-[1.3rem]  text-gray-100  font-normal  leading-[1.2] mb-4 ">
+        <p className="max-w-lg  text-[1.3rem]  text-gray-100  font-normal  leading-[1.2] mb-2 ">
           I am developer where passion lies in creating simple yet visually
           appealing interfaces.
         </p>
@@ -31,7 +31,7 @@ export default function About() {
       <motion.img
         src="/assets/avatar-pdr.png"
         alt="me"
-        className="object-fit items-center max-w-[300px] mt-4
+        className="object-fit items-center max-w-[250px] mt-6
         flex-shrink-1 object-cover transition-all flex  rounded-full "
       />
     </motion.div>

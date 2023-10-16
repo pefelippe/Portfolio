6c61cd77705cf12962c43ee4dcef1e29d437fcda
pedro felippe <pedfelippe@gmail.com>
2023-10-16 19:06:22 -0300

    minors

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import HeroContent from "../src/content/HeroContent";
 
 export default function Home() {
   return (
-    <motion.div className=" flex flex-col px-10  pt-10 pb-32 mx-auto gap-40 items-center justify-center  ">
+    <motion.div className=" flex flex-col px-10  pt-20 pb-40 mx-auto gap-40 items-center justify-center  ">
       <HeroContent />
 
       <AboutContent />

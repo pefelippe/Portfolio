f8c1a1b287bf0d75e3c33f1a06da0fde06abf036
pedro felippe <pedfelippe@gmail.com>
2024-01-05 06:00:52 -0300

    refactor AGAIN?

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
-import Links from "../Socials";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
@@ -11,14 +10,14 @@ const Header = () => {
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="flex w-screen fixed rounded-sm z-40 backdrop-blur-sm mx-auto h-16 py-4"
+      className="flex w-screen fixed rounded-sm bg-white z-40  mx-auto h-16 py-4 border-b"
     >
-      <div className="flex w-full max-w-5xl mx-auto items-center justify-between ">
+      <div className="flex w-full  mx-auto items-center justify-between px-10 max-w-5xl">
         <Link href="/">
-          <p className="text-2xl font-semibold tracking-tight">Pedro Felippe</p>
+          <p className="text-xl font-medium tracking-tight">Pedro Felippe</p>
         </Link>
 
-        <Links />
+        {/* <Links /> */}
       </div>
     </motion.header>
   );
--- src/content/ContactContent.tsx
+++ src/content/ContactContent.tsx
@@ -9,15 +9,15 @@ function ContactContent() {
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="flex justify-center w-full mx-auto  max-w-7xl py-10 xl:py-20 max-xl:max-w-3xl
+      className="flex justify-center w-full mx-auto  max-w-7xl py-10 xl:py-32 max-xl:max-w-3xl
      h-fit px-6 items-center relative  max-xl:flex-col "
     >
       <div className="relative h-full w-full flex justify-between gap-8 flex-col  items-center">
-        <div className=" flex flex-col gap-6 xl:gap-8 h-full items-center justify-between ">
-          <h1 className="text-4xl xl:text-6xl  font-bold leading-[1.2] max-w-3xl w-fit text-center">
-            I'm here to help bring your ideas to reality.
-          </h1>
-        </div>
+    
+        <h1 className="text-4xl xl:text-6xl  font-bold leading-[1.2] max-w-3xl w-fit text-center">
+          I'm here to help bring your ideas to <span className="underline underline-offset-4 decoration-blue">reality</span>.
+        </h1>
+     
         <MessageForm />
       </div>
     </motion.div>
